79bcbabee8a977dd922a47ba3287ba6cdde047b8
ivenk <ivenkoethke@hotmail.de>
2020-09-20 04:35:00 +0200

    Cleaned up imports

--- app/src/main/java/com/ivenk/bird/MatchupAdapter.kt
+++ app/src/main/java/com/ivenk/bird/MatchupAdapter.kt
@@ -1,7 +1,6 @@
 package com.ivenk.bird
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
