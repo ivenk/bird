eff3d03904edc594eb82f1ffd65a021b36a0c2bc
ivenk <ivenkoethke@hotmail.de>
2020-10-02 18:57:59 +0200

    Fixed ChampSelectFragment to properly show dropdown for text completion

--- app/src/main/java/com/ivenk/bird/ChampSelectFragment.kt
+++ app/src/main/java/com/ivenk/bird/ChampSelectFragment.kt
@@ -8,45 +8,31 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [ChampSelectFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class ChampSelectFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        println("ChampSelectFragment.onCreateView")
-
-        if (container != null) {
-            val arrayAdapter = ArrayAdapter(
-                container.context,
-                android.R.layout.select_dialog_item,
-                arrayOf("One", "Two", "Three")
-            )
-
-            println("Adapter was set")
-
-            val autoCompleteTextView = container.findViewById<AutoCompleteTextView>(R.id.champ_select_dropdown)
-            autoCompleteTextView.threshold = 1
-            autoCompleteTextView.setAdapter(arrayAdapter)
-        }
-
-
-        // Inflate the layout for this fragment
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.champ_select, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val arrayAdapter = ArrayAdapter(
+            view.context,
+            android.R.layout.select_dialog_item,
+            arrayOf("One", "Two", "Three")
+        )
+
+        val autoCompleteTextView = view.findViewById<AutoCompleteTextView>(R.id.champ_select_dropdown)
+        autoCompleteTextView.threshold = 0
+        autoCompleteTextView.setAdapter(arrayAdapter)
+        //TODO: Configure dropdown to show immediately once the text field is selected
     }
 
     companion object {
