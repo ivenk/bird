25f7af63a9e415253bfe74973e6e4035e154d2a5
ivenk <ivenkoethke@hotmail.de>
2020-10-02 19:26:47 +0200

    Implemented listeners for championselectfragments

--- app/src/main/java/com/ivenk/bird/ChampSelectFragment.kt
+++ app/src/main/java/com/ivenk/bird/ChampSelectFragment.kt
@@ -1,9 +1,11 @@
 package com.ivenk.bird
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
@@ -13,7 +15,17 @@ import androidx.fragment.app.Fragment
  * Use the [ChampSelectFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ChampSelectFragment : Fragment() {
+class ChampSelectFragment : Fragment(), AdapterView.OnItemClickListener {
+    var listener: ChampSelectListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as? ChampSelectListener
+        if (listener == null) {
+            throw ClassCastException("$context must implement OnArticleSelectedListener")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -32,11 +44,24 @@ class ChampSelectFragment : Fragment() {
         val autoCompleteTextView = view.findViewById<AutoCompleteTextView>(R.id.champ_select_dropdown)
         autoCompleteTextView.threshold = 0
         autoCompleteTextView.setAdapter(arrayAdapter)
+        autoCompleteTextView.onItemClickListener = this
         //TODO: Configure dropdown to show immediately once the text field is selected
+    }
+
+
+
+    interface ChampSelectListener {
+        fun onChampSelected(champ: String)
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = ChampSelectFragment()
     }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        println("ChampSelectFragment.onItemClick")
+        listener?.onChampSelected("sth")
+    }
+
 }
--- app/src/main/java/com/ivenk/bird/MainActivity.kt
+++ app/src/main/java/com/ivenk/bird/MainActivity.kt
@@ -3,19 +3,21 @@ package com.ivenk.bird
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(), ChampSelectFragment.ChampSelectListener {
 
-    lateinit var button: Button
+   lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.myActivity)
-        button.setOnClickListener { startActivity(Intent(this, MatchupActivity::class.java)) }
+       button = findViewById(R.id.myActivity)
+       button.setOnClickListener { startActivity(Intent(this, MatchupActivity::class.java)) }
+    }
 
-
+    override fun onChampSelected(champ: String) {
+        println("Activity received selected champion $champ")
     }
 }
